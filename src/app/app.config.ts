f785adff1e247278a41a31208589e106b0c7f087
Your Name <you@example.com>
2025-07-09 14:53:45 +0200

    edit app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
 
+// import { routes } from './app.routes';
+// import { provideClientHydration } from '@angular/platform-browser';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideRouter(routes), provideClientHydration()]
+// };
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // ✅ استيراد HttpClientModule
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule) // ✅ إضافة HttpClientModule هنا
+  ]
 };
