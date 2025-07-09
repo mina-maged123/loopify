0bf7eb2fdab8f1cadc0ae19b01ad7eebdd20efa5
MarlyMonsef <marlymonsef21@gmail.com>
2025-07-09 16:22:15 +0300

    mm

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // ✅ استيراد HttpClientModule
+import {} from '@angular/common/http'; // ✅ استيراد HttpClientModule
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -6,7 +6,11 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-request',
   standalone: true,
   imports: [FormsModule,
-    HttpClientModule],
+    
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule],
   templateUrl: './request.component.html',
   styleUrl: './request.component.css'
 })
