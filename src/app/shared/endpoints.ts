13e97796d998f76be032c20a5c9128bc762ced96
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-07-09 08:53:24 +0300

    Create the shared & service folders

---
+++ src/app/shared/endpoints.ts
@@ -0,0 +1,9 @@
+const baseUrl = "https://recyclingsystem.runasp.net/api/";
+export const ENDPOINTS = {
+    LOGIN: `${baseUrl}Account/login`,
+    REGISTER: `${baseUrl}Account/register`,
+
+
+    GET_ALL_MATERIAL: `${baseUrl}Material/all`,
+    GET_MATERIAL: (materialId:number) => `${baseUrl}Material/${materialId}`,
+}
