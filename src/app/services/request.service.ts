9973d83ad7dc1a1eb4b130001fd6cbac4fca4db8
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-07-10 22:35:17 +0300

    A small change

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -2,7 +2,7 @@ import { ICreatePickupRequest } from './../models/ICreatePickupRequest.model';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Responce } from '../models/response.model';
+import { Response } from '../models/response.model';
 import { CreatePickupRequestResponse } from '../models/CreatePickupRequestResponse';
 import { ENDPOINTS } from '../shared/endpoints';
 
@@ -13,10 +13,10 @@ export class RequestService {
 
   constructor(private http:HttpClient) { }
 
-  postPickupRequest(data:ICreatePickupRequest) : Observable<Responce<CreatePickupRequestResponse>> {
+  postPickupRequest(data:ICreatePickupRequest) : Observable<Response<CreatePickupRequestResponse>> {
     let token = localStorage.getItem('token');
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     
-    return this.http.post<Responce<CreatePickupRequestResponse>>(ENDPOINTS.POST_PICKUP_REQUEST, data, {headers: headers});
+    return this.http.post<Response<CreatePickupRequestResponse>>(ENDPOINTS.POST_PICKUP_REQUEST, data, {headers: headers});
   }
 }
