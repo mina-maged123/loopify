1129d2546e893d6cbe4f25d74dc28e58201e0115
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-07-10 22:36:16 +0300

    A small update

--- src/app/services/material.service.ts
+++ src/app/services/material.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Responce } from '../models/response.model';
 import { IMaterial } from '../models/IMaterial.model';
 import { ENDPOINTS } from '../shared/endpoints';
 
