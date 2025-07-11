66fd72e611c55340275475aca1c154755aa7d2e5
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-07-11 15:12:05 +0300

    add pickup requests component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,75 +15,76 @@ import { EmployeeLayoutComponent } from './Layouts/employee-layout/employee-layo
 
 import { RequestComponent } from './request/request.component';
 import { SubmitPickupRequestSuccessComponent } from './components/submit-pickup-request-success/submit-pickup-request-success.component';
+import { PickUpRequestComponent } from './components/Employee/pick-up-request/pick-up-request.component';
+import { ViewMapComponent } from './components/Employee/view-map/view-map.component';
+import { report } from 'process';
+import { ReportIssueComponent } from './components/Employee/report-issue/report-issue.component';
+import { ProfileComponent } from './components/Employee/profile/profile.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'forgot-password',
-        component: ForgotPasswordComponent
-    },
-    {
-        path: 'verification-code',
-        component: VerificationCodeComponent
-    },
-    {
-        path: 'reset-password',
-        component: ResetPasswordComponent
-    },
-    {
-        path: 'reset-success',
-        component: ResetSuccessComponent
-    },
-    {
-        path: 'registration-failed',
-        component: RegistrationFailedComponent
-    },
-    {
-        path: 'registration-success',
-        component: RegistrationSuccessComponent
-    },
-    {
-        path: 'gift',
-        component: GiftComponent
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: 'verification-code',
+    component: VerificationCodeComponent,
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+  },
+  {
+    path: 'reset-success',
+    component: ResetSuccessComponent,
+  },
+  {
+    path: 'registration-failed',
+    component: RegistrationFailedComponent,
+  },
+  {
+    path: 'registration-success',
+    component: RegistrationSuccessComponent,
+  },
+  {
+    path: 'gift',
+    component: GiftComponent,
+  },
+  {
+    path: 'employee',
+    component: EmployeeLayoutComponent,
+    children: [
+      { path: '', component: EmployeeDashboardComponent },
+      { path: 'pickup', component: PickUpRequestComponent },
+      { path: 'map-view', component: ViewMapComponent },
+      { path: 'report-issue',component: ReportIssueComponent,},
+      {path:'profile',component:ProfileComponent}
+    ],
+  },
 
-    }
-    ,
+  {
+    path: 'request',
+    component: RequestComponent,
+  },
+  {
+    path: 'submit-pickup-request-success',
+    component: SubmitPickupRequestSuccessComponent,
+  },
 
-    {
-        path: 'employee',
-        component: EmployeeLayoutComponent,
-        children: [
-            { path: '', component: EmployeeDashboardComponent }, // /employee
-            { path: 'pickup', component: EmployeeDashboardComponent }, // /employee/profile
-            { path: 'map-view', component: EmployeeDashboardComponent }, // /employee/settings,
-
-        ]
-    }
-
-    ,
-    {
-        path: 'request',
-        component: RequestComponent
-    },
-    {
-        path: 'submit-pickup-request-success',
-        component: SubmitPickupRequestSuccessComponent
-    },
-
-    {
-        path: '**',
-        redirectTo: ''
-    }
-
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
---
+++ src/app/components/Employee/report-issue/report-issue.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-issue',
+  imports: [],
+  templateUrl: './report-issue.component.html',
+  styleUrl: './report-issue.component.css'
+})
+export class ReportIssueComponent {
+
+}
