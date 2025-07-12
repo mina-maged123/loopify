ecfef2182677956954cdf8024d7f88001929461a
MarlyMonsef <marlymonsef21@gmail.com>
2025-07-12 23:26:53 +0300

    details

--- src/app/components/Employee/pickup-details/pickup-details.component.ts
+++ src/app/components/Employee/pickup-details/pickup-details.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pickup-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pickup-details.component.html',
   styleUrl: './pickup-details.component.css'
 })
-export class PickupDetailsComponent {
+export class AppComponent {
+  title = 'Loopify';
+  sidebarOpen = true;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
