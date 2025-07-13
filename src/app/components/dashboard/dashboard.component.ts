40f0a23d2d928c03212baf40e14a95b43015560c
AhmedIbrahimFahmy <83197446+AhmedIbrahimFahmy@users.noreply.github.com>
2025-07-13 12:22:52 +0300

    fixed the gift route in the dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -157,7 +157,7 @@ recentRequests: PickupRequest[] = [
 
   viewGifts(): void {
     // Implement view gifts functionality
-    this.router.navigate(['/gift']);
+    this.router.navigate(['customer/gift']);
     console.log('View gifts clicked');
   }
 
